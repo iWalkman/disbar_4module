026554700891867287ed5c7868fc4b7bf56ecc71
Константин Фомин <kosty.fomin98@yandex.ru>
2016-06-14 18:56:03 +0300

    Initial Commit

---
+++ discr/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  discr
+//
+//  Created by Константин on 14.06.16.
+//  Copyright © 2016 Константин. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
